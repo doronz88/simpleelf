11a6e99ca83e7989ba4f9355afd8a2f74f329bda
DoronZ <doron88@gmail.com>
2020-06-25 22:51:04 +0300

    fix python2 support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simpleelf',
-    version='0.1.7',
+    version='0.1.8',
     description='Simple ELF parser and builder',
     author='DoronZ',
     author_email='doron88@gmail.com',
@@ -10,5 +10,5 @@ setup(
     packages=find_packages(),
     install_requires=['construct>=2.10.54',
                       'pytest>=^4.6.10'],
-    python_requires='>=2.7,>=3.0'
+    python_requires='>=2.7'
 )
